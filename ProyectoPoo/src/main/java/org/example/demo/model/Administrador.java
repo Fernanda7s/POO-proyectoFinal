16b55106a34b874f03aea82e81d1954355d7c9b7
Fernandas7 <maria.rodriguez@epn.edu.ec>
2026-07-05 06:01:42 -0500

    fix:Correcion de la clase padre

--- ProyectoPoo/src/main/java/org/example/demo/model/Administrador.java
+++ ProyectoPoo/src/main/java/org/example/demo/model/Administrador.java
@@ -1,4 +1,11 @@
 package org.example.demo.model;
+// el administrado se va encargar de asignar el usuario y la contraseña se va a crear automaticamete
+// TAMBIEN EL USUARIO SE VA A CREAR AUTOMATICAMENTE
 
-public class Administrador {
+public class Administrador extends Persona {
+    public Administrador(String id, String nombre, String apellido, String usuario, String contrasenia, String cargo) {
+        super(id, nombre, apellido, usuario, contrasenia, cargo);
+    }
+
+
 }
